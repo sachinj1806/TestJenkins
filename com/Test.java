9adb718b40d89fcc837150074af990709c6a81a6
unknown <rj89862@gmail.com>
2018-01-07 21:50:59 +0530

    changes done in program i<=5

--- com/Test.java
+++ com/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 
-         for (int i=0;i <=10 ; i++) {
+         for (int i=0;i <=5 ; i++) {
         	 
         	 System.out.println(" i = " + i);
 			
