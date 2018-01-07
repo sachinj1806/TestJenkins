05d35f2713e74e53e50823ebf45aa25f3bdc33e7
unknown <rj89862@gmail.com>
2018-01-07 21:25:16 +0530

    Added Helloworld program

---
+++ com/Test.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+         for (int i=0;i <=10 ; i++) {
+        	 
+        	 System.out.println(" i = " + i);
+			
+		}
+
+	}
+
+}
